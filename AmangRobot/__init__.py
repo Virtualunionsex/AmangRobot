e8a61683e958e32aa2cf1199b74e92df7fbd5c5d
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-10-20 11:50:47 +0700

    Update __init__.py

--- AmangRobot/__init__.py
+++ AmangRobot/__init__.py
@@ -37,10 +37,10 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.INFO)
 logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLevel(logging.WARNING)
 
-LOGGER = logging.getLogger('[AmangRobot]')
-LOGGER.info("Amang is starting. | An Kennedy Project Parts. | Licensed under GPLv3.")
+LOGGER = logging.getLogger('[LuffyRobot]')
+LOGGER.info("Luffy is starting. | An Kennedy Project Parts. | Licensed under GPLv3.")
 LOGGER.info("Not affiliated to other anime or Villain in any way whatsoever.")
-LOGGER.info("Project maintained by: github.com/amangtele (t.me/amwang)")
+LOGGER.info("Project maintained by: github.com/virtualunionsex (t.me/panggil_m)")
 
 # if version < 3.9, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 8:
@@ -213,8 +213,8 @@ else:
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(2137482758)
-DEV_USERS.add(1866066766)
+DEV_USERS.add(5657257558)
+DEV_USERS.add(5860466871)
 
 if not SPAMWATCH_API:
     sw = None
